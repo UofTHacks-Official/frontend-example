460bf3edf75e79e1c91963e9a860d5a224cefd81
ShernanJ <shernanjavier@gmail.com>
2022-10-01 03:11:29 -0400

    Created config file, added loaders

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      issuer: { and: [/\.(js|ts)x?$/] },
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/media/[name].[hash:8][ext]',
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
