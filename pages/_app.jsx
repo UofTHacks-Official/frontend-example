48652e867394e56ce6e7fa27927de780ecc7977a
ShernanJ <shernanjavier@gmail.com>
2022-10-01 03:15:32 -0400

    Added global styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,16 @@
+import { darkTheme, globalStyles } from "~stitches";
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  globalStyles();
+  return (
+    <>
+      <Head>
+        <title>UofTHacks X</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
