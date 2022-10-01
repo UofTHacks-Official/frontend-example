2c2d66b9ec65f3c50338c7304fca2c62a75df74a
ShernanJ <shernanjavier@gmail.com>
2022-10-01 03:15:48 -0400

    Updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import VectorLights from '../components/VectorLights';
+import VectorLights from '@components/VectorLights';
 import { styled } from '@stitches/react';
+import { Button, Body, DisplayText, Title, Header, Subtitle } from '@components/atoms';
 
 const Wrapper = styled('main', {
   minHeight: "100vh",
+  minWidth: "100%",
   margin: "0",
   backgroundColor: '#0D0D0D',
+  // backgroundColor: 'white',
 })
-
-const Header = styled('h1', {
-  color: 'red',
-  margin: '2rem',
-});
 
 export default function Home() {
   return (
     <Wrapper>
-      {/* <Header>Hi this is an example project!</Header> */}
-      Test
+      <Button>Hello</Button>
+      <Button type="cta" title="test"></Button>
+      <DisplayText>UofTHacks</DisplayText>
+      <Title>There's no place like home</Title>
+      <Header type="h1"></Header>
+      <Header type="h2"></Header>
+      <Subtitle>Subtitle</Subtitle>
+      <Body size="large">Body Large</Body>
+      <Body>Body Regular</Body>
+      <Body size="small">Body Small</Body>
       <VectorLights></VectorLights>
+      test
     </Wrapper>
   );
 }
