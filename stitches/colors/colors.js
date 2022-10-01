4a5e855358828525b4cdad7aa0d71c167e7c631a
ShernanJ <shernanjavier@gmail.com>
2022-10-01 15:52:49 -0400

    Fixed colour code typo

--- stitches/colors/colors.js
+++ stitches/colors/colors.js
@@ -3,7 +3,7 @@ export const colors = {
   secondary: '#EB9C5C',
   tertiary: '#B487EE',
   quaternary: '#DF7D7D',
-  'primary-text': 'F9F9F9',
+  'primary-text': '#F9F9F9',
   'alternate-text': '#242424',
   'secondary-text': '#8F8F8F',
 };
