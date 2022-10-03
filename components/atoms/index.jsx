f15c52614ba794cde6d428ad8e12cc398c4c4411
ShernanJ <shernanjavier@gmail.com>
2022-10-03 04:00:38 -0400

    Added MLH Banner

--- components/atoms/index.jsx
+++ components/atoms/index.jsx
@@ -1,3 +1,4 @@
-export * from "./Button";
-export * from "./Text";
-export * from "./VectorLights";
+export * from './Button';
+export * from './Text';
+export * from './Background';
+export * from "./MLHBanner";
