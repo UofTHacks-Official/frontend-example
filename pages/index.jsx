6c6e64f1877f4c7c768a12fbb88a172fb105727c
ShernanJ <shernanjavier@gmail.com>
2022-10-03 04:01:39 -0400

    Made lots of changes, too many to list and will be overwritten soon lol

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,90 @@
 import React from 'react';
-import { styled } from '@stitches/react';
-import { Button, Body, DisplayText, Title, Header, Subtitle, MainBackground, StarsBackground} from '@components/atoms';
+import Image from 'next/image';
+import { styled, css } from '~stitches';
+import { Button, Body, DisplayText, Title, Header, Subtitle, MainBackground, StarsBackground, MLHBanner} from '@components/atoms';
+import { About } from "@components/sections";
 
-const Test = styled('div', {
-  height: "5000px",
+import AboutUsImg from "@assets/AboutUs.png";
+
+const HeroWrapper = styled('div', {
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "100vh",
   width: "100%",
 })
 
+const AboutWrapper = styled('section', {
+  minHeight: "100vh",
+  width: "100%",
+})
+
+const ContentWrapper = styled('div', css({
+  display: 'flex',
+  maxWidth: "1440px",
+  flexFlow: "row wrap",
+  gap: "5vw",
+}));
+
+const ImageWrapper = styled('div', css({
+  position: "relative",
+  maxWidth: "562px",
+  maxHeight: "309px",
+  minWidth: "300px",
+  height: "309px",
+  flex: "1",
+  overflow: "hidden",
+  "& img": {
+    boxShadow: "0px 8px 24px 2px rgba(0, 0, 0, 0.25)",
+    borderRadius: "10px",
+    margin: "auto",
+  }
+}));
+
+const TextWrapper = styled('div', css({
+  display: "flex",
+  flexDirection: "column",
+  minWidth: "300px",
+  flex: "1"
+}));
+
+
 export default function Home() {
   return (
-    <MainBackground>
-      <Button>Hello</Button>
-      <Button type="cta" title="test"></Button>
-      <DisplayText>UofTHacks X</DisplayText>
-      <Title>There's no place like home</Title>
-      <Header type="h1"></Header>
-      <Header type="h2"></Header>
-      <Subtitle>Subtitle</Subtitle>
-      <Body size="large">Body Large</Body>
-      <Body>Body Regular</Body>
-      <Body size="small">Body Small</Body>
-    </MainBackground>
+    <>
+      <MLHBanner />
+      <MainBackground padded>
+          <HeroWrapper>
+            <Subtitle>
+              January 20-22, 2023&nbsp;&nbsp;|
+              <span
+                style={{'color': '#DF7D7D'}}
+              >
+              &nbsp;&nbsp;Toronto, ON.
+              </span>
+            </Subtitle>
+            <DisplayText>UofTHacks X</DisplayText>
+            <Body>Interested in sponsoring?</Body>
+          </HeroWrapper>
+          <AboutWrapper>
+            <Title>About Us</Title>
+            <ContentWrapper>
+              <TextWrapper>
+                <Body>
+                Canada's first student-run hackathon, UofTHacks, is back for its 10th consecutive year on January 20-22, 2023!
+                </Body>
+                <Body>
+                To celebrate this milestone, we will be even bigger and better than ever before! Over 600 innovators, developers, designers, and entrepreneurs will participate in a 36-hour competition entirely in-person, where they will build unique and impactful projects.
+                </Body>
+                <Body>
+                We strive to provide a space where hackers can discover their passions, and we want your support in helping these hackers shape the future!
+                </Body>
+              </TextWrapper>
+              <ImageWrapper>
+                <Image src={AboutUsImg} layout="fill" objectFit='contain' />
+              </ImageWrapper>
+            </ContentWrapper>
+          </AboutWrapper>
+      </MainBackground>
+    </>
   );
 }
