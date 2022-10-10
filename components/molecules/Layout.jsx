f74232fb81af28daa47ee2f82ee6813c4d8f1b86
ShernanJ <shernanjavier@gmail.com>
2022-10-09 20:52:53 -0400

    Added layout wrapper component

--- components/molecules/Layout.jsx
+++ components/molecules/Layout.jsx
@@ -1,2 +1,13 @@
-import {styled} from "~stitches";
+import { styled, css } from "~stitches";
 
+import { Footer } from "@components/molecules";
+
+export const Layout = (props) => {
+
+  return (
+    <>
+      {props.children}
+      <Footer />
+    </>
+  );
+}
