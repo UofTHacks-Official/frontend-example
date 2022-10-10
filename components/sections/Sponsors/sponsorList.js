af028f0dc43233ce9889d036530a984d1d253db5
ShernanJ <shernanjavier@gmail.com>
2022-10-09 22:42:26 -0400

    List render all sponsors from a file

---
+++ components/sections/Sponsors/sponsorList.js
@@ -0,0 +1,46 @@
+import Accenture from '@assets/Sponsors/Accenture.svg';
+
+export const sponsors = {
+  diamond: [
+    {
+      name: 'Accenture',
+      url: 'https://www.accenture.com/',
+      img: Accenture,
+    },
+  ],
+  platinum: [
+    {
+      name: 'Accenture',
+      url: 'https://www.accenture.com/',
+      img: Accenture,
+    },
+  ],
+  gold: [
+    {
+      name: 'Accenture',
+      url: 'https://www.accenture.com/',
+      img: Accenture,
+    },
+  ],
+  silver: [
+    {
+      name: 'Accenture',
+      url: 'https://www.accenture.com/',
+      img: Accenture,
+    },
+  ],
+  bronze: [
+    {
+      name: 'Accenture',
+      url: 'https://www.accenture.com/',
+      img: Accenture,
+    },
+  ],
+  inkind: [
+    {
+      name: 'Accenture',
+      url: 'https://www.accenture.com/',
+      img: Accenture,
+    },
+  ],
+};
